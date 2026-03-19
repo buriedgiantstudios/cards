be78d7dcacc6470baff355330b0c36735d7271c1
Kyle J. Kemp <kyle@seiyria.com>
2026-03-19 13:59:43 +0000

    buriedgiantstudios.com -> buriedgiant.com

--- scripts/build-card-data.ts
+++ scripts/build-card-data.ts
@@ -17,7 +17,7 @@ const formatCard = (card) => {
   }
 
   const baseImageName = card.image;
-  card.image = `https://cardcdn.buriedgiantstudios.com/cards/${card.game}/${
+  card.image = `https://cardcdn.buriedgiant.com/cards/${card.game}/${
     card.locale
   }/${encodeURIComponent(baseImageName)}.webp`;
 
